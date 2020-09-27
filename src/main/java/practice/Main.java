edcb60868b5c82b2c58e80db6c0357a16b1e0c78
RayDarar <dfqgth400@gmail.com>
2020-09-27 23:13:47 +0600

    work: defining jstl, mysql connector

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,7 +1,6 @@
 package practice;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,25 +13,9 @@ public class Main extends HttpServlet {
 	private static final long serialVersionUID = -4751096228274971485L;
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		PrintWriter writer = res.getWriter();
-		res.setHeader("Content-Type", "text/html");
-
-		String username = req.getParameter("username");
-
-		if (username == "RayDarar") {
-			writer.write("<h1>Welcome!</h1>");
-			return;
-		}
-
-
-		res.sendError(404);
-	}
-
-	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		int result = 1 / 0;
 
-		res.getWriter().write("Wow" + result);
+		req.getSession().setAttribute("users", new String[] { "Dave", "Bob", "Dylan" });
+		req.getRequestDispatcher("/user.jsp").forward(req, res);
 	}
 }
--- src/main/webapp/user.jsp
+++ src/main/webapp/user.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,11 +8,10 @@
     <title>Document</title>
   </head>
   <body>
-    <form action="test" method="POST">
-      <input type="text" name="username" />
-      <input type="text" name="password" />
-
-      <input type="submit" value="Login" />
-    </form>
+    <ul>
+      <c:forEach var="user" items="${users}">
+        <li>${user}</li>
+      </c:forEach>
+    </ul>
   </body>
 </html>
