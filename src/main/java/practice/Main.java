b8af0c6b9c1613941cc25812c0c39ff89b489ca1
RayDarar <dfqgth400@gmail.com>
2020-09-27 16:12:41 +0600

    work: project initialized

---
+++ src/main/java/practice/Main.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = -4751096228274971485L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter res = response.getWriter();
+		response.setHeader("Content-Type", "text/html");
+
+		res.write("Hello, World!");
+	}
+}
